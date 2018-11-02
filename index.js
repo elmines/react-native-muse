f0901623f6a48b52c86d1c1c0b9ffcc61af2b0c1
Ethan Mines <elmines325@gmail.com>
2018-11-02 17:20:38 -0500

    Add connections() observable

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //@flow
 import { NativeModules, DeviceEventEmitter } from 'react-native';
-import type {DeviceManager} from "react-native-bci";
-import type {DataPacket} from "react-native-bci";
+import type {DeviceManager, DataPacket, ConnectionPacket, ConnectStatus} from "react-native-bci";
 import {Observable, Observer} from "rxjs";
 import Singleton from "flow-singleton";
 
@@ -25,7 +24,8 @@ export class MuseDeviceManager implements DeviceManager
     });
     return packetStream;
   }
-  devices(): Observable<Array<string>> {return MuseDeviceManager.devicesObservable.getInstance();}
+  devices(): Observable<Array<string>> {return this.devicesObservable.getInstance();}
+  connections(): Observable<ConnectionPacket> {return this.connectionsObservable.getInstance();}
   connect(museID: string) : void {RNLibMuse.connect(museID);}
   startListening(): void {RNLibMuse.startListening();}
   stopListening(): void {RNLibMuse.stopListening();}
@@ -34,23 +34,11 @@ export class MuseDeviceManager implements DeviceManager
   search(): void {this.stopListening(); this.startListening();}
 
 
-
-
   //PRIVATE, STATIC
   static channelNames: Array<string> = RNLibMuse.getChannelNames();
   static samplingRate: number = 256; //TODO: Get this from the underlying native module
 
-  static devicesObservable: Singleton<Observable<Array<string>>> =
-    new Singleton((): Observable<Array<string>> =>
-    {
-    	  return Observable.create((observer: Observer): void =>
-	  {
-		  DeviceEventEmitter.addListener("OnMuseListChanged", (muses: Array<string>): void =>
-		  {
-			  observer.next(muses);
-		  });
-	  });
-    });
+
 
   static instance: Singleton<MuseDeviceManager> = new Singleton((): MuseDeviceManager =>
   {
@@ -70,13 +58,28 @@ export class MuseDeviceManager implements DeviceManager
   }
 
   //PRIVATE, INSTANCE
+  devicesObservable: Singleton<Observable<Array<string>>>;
+  connectionsObservable: Singleton<Observable<ConnectionPacket>>;
   constructor()
   {
     if (MuseDeviceManager.instance.isInitialized())
       throw "Error: There can only be one MuseDeviceManager";
     RNLibMuse.Init();
     RNLibMuse.setBufferSize(64);
-  }
+    this.devicesObservable = new Singleton((): Observable<Array<string>> => {
+      return Observable.create((observer: Observer): void => {
+        DeviceEventEmitter.addListener("OnMuseListChanged", (muses: Array<string>): void => {
+          observer.next(muses);
+        });
+  	  });
+    });
+    this.connectionsObservable = new Singleton((): Observable<ConnectionPacket> => {
+        return Observable.create((observer: Observer): void => {
+          DeviceEventEmitter.addListener("ChangeMuseConnectionState",
+            ([id: string, status: ConnectStatus]): void => {observer.next({id, status});}
+          );
+        });
+    });
+  }//End constructor
 
-
-}
+}//End MuseDeviceManager
